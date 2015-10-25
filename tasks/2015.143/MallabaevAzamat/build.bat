85e47662f5066770fca3ef7388cd62735daafaff
Mallabaev Azamat <home9428@yandex.ru, mallabaev.an@yandex.ru>
2015-10-25 22:22:44 +0300

    Updated build script Add command with removing and creating directory

--- tasks/2015.143/MallabaevAzamat/build.bat
+++ tasks/2015.143/MallabaevAzamat/build.bat
@@ -2,7 +2,10 @@
 set CCD=%CD%
 set CFN=%0
 cd %CFN:~0,-9%
-@echo on
+
+rmdir /S /Q %CD%\fharp
+mkdir %CD%\fharp
+echo fstar.exe --codegen FSharp VList.fst VDigit.fst VNatural.fst
 %fstar% --smt %z3% --codegen FSharp --odir %CD%\fsharp %CD%\fstar\VList.fst %CD%\fstar\VDigit.fst %CD%\fstar\VNatural.fst"
-@echo off
+
 cd %CCD%
