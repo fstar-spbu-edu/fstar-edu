447ae68a1435eeccf405341651779e7643eb9689
Mallabaev Azamat <home9428@yandex.ru, mallabaev.an@yandex.ru>
2015-10-25 22:28:58 +0300

    Updated bulid script

--- tasks/2015.143/MallabaevAzamat/build.bat
+++ tasks/2015.143/MallabaevAzamat/build.bat
@@ -3,8 +3,8 @@ set CCD=%CD%
 set CFN=%0
 cd %CFN:~0,-9%
 
-rmdir /S /Q %CD%\fharp
-mkdir %CD%\fharp
+rmdir /S /Q %CD%\fsharp
+mkdir %CD%\fsharp
 echo fstar.exe --codegen FSharp VList.fst VDigit.fst VNatural.fst
 %fstar% --smt %z3% --codegen FSharp --odir %CD%\fsharp %CD%\fstar\VList.fst %CD%\fstar\VDigit.fst %CD%\fstar\VNatural.fst"
 
