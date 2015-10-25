4b2ab38838102b530a639964b7ac39288042143e
Mallabaev Azamat <home9428@yandex.ru, mallabaev.an@yandex.ru>
2015-10-25 22:04:10 +0300

    Updated build script Added fsharp sources generation

--- tasks/2015.143/MallabaevAzamat/build.bat
+++ tasks/2015.143/MallabaevAzamat/build.bat
@@ -3,6 +3,6 @@ set CCD=%CD%
 set CFN=%0
 cd %CFN:~0,-9%
 @echo on
-%fstar% --smt %z3% --odir %CD%\fsharp %CD%\fstar\VList.fst %CD%\fstar\VDigit.fst %CD%\fstar\VNatural.fst"
+%fstar% --smt %z3% --codegen FSharp --odir %CD%\fsharp %CD%\fstar\VList.fst %CD%\fstar\VDigit.fst %CD%\fstar\VNatural.fst"
 @echo off
 cd %CCD%
